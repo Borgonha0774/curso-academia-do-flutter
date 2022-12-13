dee8c770f84ce2bdd830184949a986c2cda9a68f
borgonha <amiltonborgonha.exis@gmail.com>
2022-12-13 20:20:33 -0300

    logo

--- provider sqlite module/todo_list_provider/lib/app/core/widget/todo_list_logo.dart
+++ provider sqlite module/todo_list_provider/lib/app/core/widget/todo_list_logo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
 
 class TodoListLogo extends StatelessWidget {
   const TodoListLogo({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class TodoListLogo extends StatelessWidget {
           'assets/logo.png',
           height: 200,
         ),
-        Text('Todo List', style: Theme.of(context).textTheme.headline6),
+        Text('Todo List', style: context.textTheme.headline6),
       ],
     );
   }
