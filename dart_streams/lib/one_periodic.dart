74c39ab66a94ab311a7c4204dc899ba15ce5122b
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-12-13 09:58:06 -0300

    await for

---
+++ dart_streams/lib/one_periodic.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: avoid_print
+
+void main(List<String> args) async {
+  /* Para criar uma Stream precisamos de um controller com excessão do periodic*/
+
+  const interval = Duration(microseconds: 1);
+  /* Um callback apenas para termos um valor */
+  final stream = Stream<int>.periodic(interval, callback);
+
+  /*Este tipo de for ele é ativado qdo o periodic enviar os dados é diferente de um loop infinito  */
+  await for (var i in stream) {
+    print(i);
+  }
+}
+
+int callback(int value) {
+  return (value + 1) * 2;
+}
